19bfcf4991a14247668bb22749740da295d21a75
StellarisJAY <760409558@qq.com>
2022-06-22 14:55:50 +0800

    feat: 添加启动服务的主函数

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
-package redigo
+package main
+
+import (
+	"fmt"
+	"redigo/database"
+	"redigo/tcp"
+)
+
+var banner = ` 
+________     ____________________________ 
+___  __ \__________  /__(_)_  ____/_  __ \
+__  /_/ /  _ \  __  /__  /_  / __ _  / / /
+_  _, _//  __/ /_/ / _  / / /_/ / / /_/ / 
+/_/ |_| \___/\__,_/  /_/  \____/  \____/
+                             v1.0-SNAPSHOT`
+
+func main() {
+	fmt.Println(banner)
+	db := database.NewSingleDB(0, 102400)
+	server := tcp.NewServer(":6380", db)
+	err := server.Start()
+	if err != nil {
+		panic(err)
+	}
+}
