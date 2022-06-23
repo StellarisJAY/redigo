c28b2e6d69802f092b48cdb72262cd8194c861d2
StellarisJAY <760409558@qq.com>
2022-06-23 21:20:56 +0800

    feat: 添加错误类型，WrongOperationType

--- redis/protocol/errors.go
+++ redis/protocol/errors.go
@@ -6,10 +6,11 @@ import (
 )
 
 var (
-	WrongArgumentNumberError = "-ERR wrong number of arguments for '%s' command"
-	UnknownCommandError      = "-ERR unknown command '%s'"
-	HashValueNotIntegerError = errors.New("-ERR hash value is not an integer")
-	ProtocolError            = []byte("-Error Wrong protocol")
+	WrongArgumentNumberError = "ERR wrong number of arguments for '%s' command"
+	UnknownCommandError      = "ERR unknown command '%s'"
+	HashValueNotIntegerError = errors.New("ERR hash value is not an integer")
+	ProtocolError            = []byte("Error Wrong protocol")
+	WrongTypeOperationError  = errors.New("WRONGTYPE Operation against a key holding the wrong kind of value")
 )
 
 func CreateWrongArgumentNumberError(command string) error {
