27bf95ba31c5b82860e9b16da00713ee30b1214b
StellarisJAY <760409558@qq.com>
2022-06-24 12:16:54 +0800

    feat: 添加RPUSH和RPOP命令

--- database/list.go
+++ database/list.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"log"
 	"redigo/datastruct/list"
 	"redigo/redis"
 	"redigo/redis/protocol"
@@ -10,6 +11,8 @@ import (
 func init() {
 	RegisterCommandExecutor("lpush", execLPush)
 	RegisterCommandExecutor("lpop", execLPop)
+	RegisterCommandExecutor("rpush", execRPush)
+	RegisterCommandExecutor("rpop", execRPop)
 }
 
 func execLPush(db *SingleDB, command redis.Command) *protocol.Reply {
@@ -57,6 +60,53 @@ func execLPop(db *SingleDB, command redis.Command) *protocol.Reply {
 			}
 		} else {
 			return protocol.NewErrorReply(protocol.WrongTypeOperationError)
+		}
+	}
+	return protocol.NilReply
+}
+
+func execRPush(db *SingleDB, command redis.Command) *protocol.Reply {
+	args := command.Args()
+	if len(args) < 2 {
+		return protocol.NewErrorReply(protocol.CreateWrongArgumentNumberError("rpush"))
+	}
+	key := string(args[0])
+	v, exists := db.data.Get(key)
+	var linkedList *list.LinkedList
+	if exists {
+		entry := v.(*Entry)
+		if reflect.TypeOf(entry.Data).String() != "*list.LinkedList" {
+			return protocol.NewErrorReply(protocol.WrongTypeOperationError)
+		}
+		linkedList = entry.Data.(*list.LinkedList)
+	} else {
+		linkedList = list.NewLinkedList()
+		entry := &Entry{Data: linkedList}
+		db.data.Put(key, entry)
+	}
+	for _, arg := range args[1:] {
+		linkedList.AddRight(arg)
+	}
+	return protocol.NewNumberReply(linkedList.Size())
+}
+
+func execRPop(db *SingleDB, command redis.Command) *protocol.Reply {
+	args := command.Args()
+	if len(args) != 1 {
+		return protocol.NewErrorReply(protocol.CreateWrongArgumentNumberError("rpop"))
+	}
+	key := string(args[0])
+	v, exists := db.data.Get(key)
+	if exists {
+		entry := v.(*Entry)
+		if reflect.TypeOf(entry.Data).String() != "*list.LinkedList" {
+			log.Println("Type of entry: ", reflect.TypeOf(entry.Data).String())
+			return protocol.NewErrorReply(protocol.WrongTypeOperationError)
+		}
+		linkedList := entry.Data.(*list.LinkedList)
+		right := linkedList.RemoveRight()
+		if right != nil {
+			return protocol.NewBulkValueReply(right)
 		}
 	}
 	return protocol.NilReply
