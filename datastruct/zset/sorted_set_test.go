fa462fa01e8505646504f9212238b072e0243835
StellarisJAY <760409558@qq.com>
2022-06-27 17:01:11 +0800

    test: 添加ZSet数据结构PopMax和PopMin的测试用例

--- datastruct/zset/sorted_set_test.go
+++ datastruct/zset/sorted_set_test.go
@@ -30,3 +30,21 @@ func TestSortedSet_Rank(t *testing.T) {
 		}
 	}
 }
+
+func TestSortedSet_PopMax(t *testing.T) {
+	set := initTest(100)
+	for i := 100; i >= 1; i-- {
+		if e := set.PopMax(); e == nil || e.Member != strconv.Itoa(i) || int(e.Score) != i {
+			t.Fail()
+		}
+	}
+}
+
+func TestSortedSet_PopMin(t *testing.T) {
+	set := initTest(100)
+	for i := 1; i <= 100; i++ {
+		if e := set.PopMin(); e == nil || e.Member != strconv.Itoa(i) || int(e.Score) != i {
+			t.Fail()
+		}
+	}
+}
