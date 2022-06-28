6f8e2106b7227b578ae2eea2cf42e25cc6f0bfe0
StellarisJAY <760409558@qq.com>
2022-06-28 11:59:07 +0800

    feat: 添加SortedSet数据结构的ZRANGE命令

--- database/sorted_set.go
+++ database/sorted_set.go
@@ -14,6 +14,8 @@ func init() {
 	RegisterCommandExecutor("zrank", execZRank)
 	RegisterCommandExecutor("zpopmin", execPopMin)
 	RegisterCommandExecutor("zpopmax", execPopMax)
+	RegisterCommandExecutor("zcard", execZCard)
+	RegisterCommandExecutor("zrange", execZRange)
 }
 
 func execZAdd(db *SingleDB, command redis.Command) *protocol.Reply {
@@ -164,6 +166,66 @@ func execPopMin(db *SingleDB, command redis.Command) *protocol.Reply {
 				result[j] = min.Member
 				result[j+1] = strconv.FormatFloat(min.Score, 'f', -1, 64)
 				j += 2
+			}
+		}
+		return protocol.NewStringArrayReply(result)
+	}
+	return protocol.EmptyListReply
+}
+
+func execZCard(db *SingleDB, command redis.Command) *protocol.Reply {
+	args := command.Args()
+	if len(args) != 1 {
+		return protocol.NewErrorReply(protocol.CreateWrongArgumentNumberError("ZCARD"))
+	}
+	sortedSet, err := getSortedSet(db, string(args[0]))
+	if err != nil {
+		return protocol.NewErrorReply(err)
+	}
+	if sortedSet != nil {
+		return protocol.NewNumberReply(sortedSet.Size())
+	}
+	return protocol.NewNumberReply(0)
+}
+
+func execZRange(db *SingleDB, command redis.Command) *protocol.Reply {
+	args := command.Args()
+	if len(args) < 3 {
+		return protocol.NewErrorReply(protocol.CreateWrongArgumentNumberError("ZRANGE"))
+	}
+	// parse start and end values
+	start, err1 := strconv.Atoi(string(args[1]))
+	end, err2 := strconv.Atoi(string(args[2]))
+	if err1 != nil || err2 != nil {
+		return protocol.NewErrorReply(protocol.HashValueNotIntegerError)
+	}
+	withScores := false
+	if len(args) == 4 && string(args[3]) == "WITHSCORES" {
+		withScores = true
+	}
+	// get sorted set structure
+	sortedSet, err := getSortedSet(db, string(args[0]))
+	if err != nil {
+		return protocol.NewErrorReply(err)
+	}
+	if sortedSet != nil {
+		elements := sortedSet.Range(start, end)
+		if elements == nil {
+			return protocol.EmptyListReply
+		}
+		var result []string
+		if withScores {
+			result = make([]string, len(elements)*2)
+			i := 0
+			for _, e := range elements {
+				result[i] = e.Member
+				result[i+1] = strconv.FormatFloat(e.Score, 'f', -1, 64)
+				i += 2
+			}
+		} else {
+			result = make([]string, len(elements))
+			for i, e := range elements {
+				result[i] = e.Member
 			}
 		}
 		return protocol.NewStringArrayReply(result)
