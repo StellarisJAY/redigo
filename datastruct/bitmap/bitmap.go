884f7f3203d200fdf496015db832c71c5a876639
StellarisJAY <760409558@qq.com>
2022-07-01 11:02:19 +0800

    feat: 添加BitMap数据结构

---
+++ datastruct/bitmap/bitmap.go
@@ -0,0 +1,53 @@
+package bitmap
+
+import "fmt"
+
+// BitMap data structure is the same with normal byte slice
+type BitMap []byte
+
+func New() *BitMap {
+	b := BitMap(make([]byte, 0))
+	return &b
+}
+
+// each slot contains 8 bits, this function tells the target offset's slot
+func getSlot(offset int64) int64 {
+	if offset%8 == 1 {
+		return offset / 8
+	}
+	return offset/8 + 1
+}
+
+// if offset is greater than bitMap's slice size, expand slice
+func (b *BitMap) grow(size int64) {
+	i := size - int64(len(*b))
+	if i <= 0 {
+		return
+	}
+	*b = append(*b, make([]byte, i)...)
+}
+
+func (b *BitMap) SetBit(offset int64, bit byte) byte {
+	slot := getSlot(offset)
+	offset0 := offset % 8
+	b.grow(slot + 1)
+	mask := bit << offset0
+	original := (*b)[slot] >> offset0 & 0x01
+	(*b)[slot] = (*b)[slot] | mask
+	return original
+}
+
+func (b *BitMap) GetBit(offset int64) byte {
+	slot := getSlot(offset)
+	if slot >= int64(len(*b)) {
+		return 0
+	}
+	offset0 := offset % 8
+	return ((*b)[slot] >> offset0) & 0x01
+}
+
+func (b *BitMap) printBits() {
+	for _, bit := range *b {
+		fmt.Println(bit)
+	}
+}
