9ee6fa333bad6106d376424bc235428db3931054
StellarisJAY <760409558@qq.com>
2022-07-12 12:01:37 +0800

    feat: 新增RDB文件读写

--- rdb/codec/decoder.go
+++ rdb/codec/decoder.go
@@ -3,15 +3,28 @@ package codec
 import (
 	"bufio"
 	"encoding/binary"
+	"errors"
 	"fmt"
 )
 
 const (
+	EOF          = byte(0xff)
+	SelectDB     = byte(0xfe)
+	ExpireTime   = byte(0xfd)
+	ExpireTimeMs = byte(0xfc)
+	ReSizeDB     = byte(0xfb)
+	AUX          = byte(0xfa)
+
 	StringType    = byte(0x00)
 	ListType      = byte(0x01)
 	SetType       = byte(0x02)
 	SortedSetType = byte(0x03)
 	HashType      = byte(0x04)
+)
+
+var (
+	MagicNum = []byte{52, 45, 44, 49, 53}
+	Version  = []byte("0007")
 )
 
 type Decoder struct {
@@ -20,6 +33,11 @@ type Decoder struct {
 
 func NewDecoder(reader *bufio.Reader) *Decoder {
 	return &Decoder{reader: reader}
+}
+
+func (dec *Decoder) Read(buf []byte) error {
+	_, err := dec.reader.Read(buf)
+	return err
 }
 
 // read an int length value from reader
@@ -57,4 +75,49 @@ func (dec *Decoder) readLength() (uint64, bool, error) {
 		length = uint64(firstByte)
 	}
 	return length, special, nil
+}
+
+func (dec *Decoder) ReadByte() (byte, error) {
+	return dec.reader.ReadByte()
+}
+
+// ReadTTL TTL time
+func (dec *Decoder) ReadTTL() (uint64, error) {
+	buf := make([]byte, 8)
+	err := dec.Read(buf)
+	if err != nil {
+		return 0, err
+	}
+	return binary.BigEndian.Uint64(buf), nil
+}
+
+// ReadDBIndex database index
+func (dec *Decoder) ReadDBIndex() (int, error) {
+	length, special, err := dec.readLength()
+	if err != nil {
+		return 0, err
+	}
+	if special {
+		return 0, errors.New("RDB read db index error: wrong type")
+	}
+	return int(length), nil
+}
+
+// ReadDBSize database size and ttl keys size
+func (dec *Decoder) ReadDBSize() (uint64, uint64, error) {
+	size, special, err := dec.readLength()
+	if err != nil {
+		return 0, 0, err
+	}
+	if special {
+		return 0, 0, errors.New("RDB read db index error: wrong type")
+	}
+	ttlSize, special, err := dec.readLength()
+	if err != nil {
+		return 0, 0, err
+	}
+	if special {
+		return 0, 0, errors.New("RDB read db index error: wrong type")
+	}
+	return size, ttlSize, nil
 }
