d88e44db45418e18e62f0135c33723a75e004eaf
StellarisJAY <760409558@qq.com>
2022-09-06 11:28:42 +0800

    feat: buffer接口

---
+++ util/buffer/buffer.go
@@ -0,0 +1,29 @@
+package buffer
+
+type Buffer interface {
+	// Read from buffer, this method extends io.Reader
+	Read([]byte) (int, error)
+	// ReadUntil read from the current read index until the delim byte
+	ReadUntil(delim byte) ([]byte, error)
+	// Next returns a slice of N bytes starting from current read index
+	Next(n int) ([]byte, error)
+	// Skip n bytes
+	Skip(n int) error
+	// ReadByte reads only one byte from read index
+	ReadByte() (byte, error)
+
+	// Write bytes to buffer, io.Writer
+	Write([]byte) (int, error)
+	// WriteString writes a string to buffer
+	WriteString(s string) error
+	// WriteByte writes a single byte to buffer
+	WriteByte(b byte) error
+
+	Len() int
+	Size() int
+	// Available get the available space in buffer
+	Available() int
+}
+
+const EmptyBufferSize = 256
+const MaxBufferSize = 16 * 1024
