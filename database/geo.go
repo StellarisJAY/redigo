6acd1a1cbd67d496160645981c9c6099f56abb15
StellarisJAY <760409558@qq.com>
2022-11-24 16:35:01 +0800

    feat: georadius命令

--- database/geo.go
+++ database/geo.go
@@ -13,6 +13,7 @@ func init() {
 	RegisterCommandExecutor("GEOPOS", execGeoPos, -2)
 	RegisterCommandExecutor("GEODIST", execGeoDist, -3)
 	RegisterCommandExecutor("GEOHASH", execGeoHash, -2)
+	RegisterCommandExecutor("GEORADIUS", execGeoRadius, -4)
 }
 
 func execGeoAdd(db *SingleDB, command redis.Command) *redis.RespCommand {
@@ -154,4 +155,108 @@ func execGeoHash(db *SingleDB, command redis.Command) *redis.RespCommand {
 		}
 	}
 	return redis.NewNestedArrayCommand(result)
+}
+
+func execGeoRadius(db *SingleDB, command redis.Command) *redis.RespCommand {
+	args := command.Args()
+	if !ValidateArgCount(command.Name(), len(args)) {
+		return redis.NewErrorCommand(redis.CreateWrongArgumentNumberError("GEORADIUS"))
+	}
+	key := string(args[0])
+	// 解析经纬度数值 和 范围数值
+	longitude, err := strconv.ParseFloat(string(args[1]), 64)
+	if err != nil {
+		return redis.NewErrorCommand(redis.ValueNotFloatError)
+	}
+	latitude, err := strconv.ParseFloat(string(args[2]), 64)
+	if err != nil {
+		return redis.NewErrorCommand(redis.ValueNotFloatError)
+	}
+	radius, err := strconv.ParseFloat(string(args[3]), 64)
+	if err != nil {
+		return redis.NewErrorCommand(redis.ValueNotFloatError)
+	}
+	// 经纬度不符合范围，返回坐标错误
+	if (longitude < -180 || longitude > 180) || (latitude < -90 || latitude > 90) {
+		return redis.NewErrorCommand(redis.CreateInvalidCoordinatePairError(longitude, latitude))
+	}
+	var unitFactor float64 = 1
+	withCoord, withDist, withHash, _ := false, false, false, false
+	if len(args) > 4 {
+		// 额外参数解析
+		extraArgs := args[4:]
+		for _, arg := range extraArgs {
+			switch string(arg) {
+			// 单位换算因子
+			case "m":
+				unitFactor = 1
+			case "km":
+				unitFactor = 1000
+			case "mi":
+				unitFactor = 1 / 0.00062137
+			case "ft":
+				unitFactor = 1 / 3.2808399
+			// 返回额外信息参数
+			case "WITHCOORD":
+				withCoord = true
+			case "WITHDIST":
+				withDist = true
+			case "WITHHASH":
+				withHash = true
+			case "DESC":
+				// todo 结果排序
+			default:
+				return redis.NewErrorCommand(redis.SyntaxError)
+			}
+		}
+	}
+	radius = radius * unitFactor
+	sortedSet, err := getSortedSet(db, key)
+	if err != nil {
+		return redis.NewErrorCommand(err)
+	}
+	if sortedSet == nil {
+		return redis.NilCommand
+	}
+	// 获取周围的元素
+	elements := geoRadius0(sortedSet, latitude, longitude, radius)
+	result := make([][]byte, len(elements))
+	for i, element := range elements {
+		// 单个element的结果是一个嵌套的数组
+		var nestedResult [][]byte
+		buf := geo.FromUint64(uint64(element.Score))
+		// 解码geoHash值
+		lat, lng, _ := geo.Decode(buf)
+		nestedResult = append(nestedResult, redis.Encode(redis.NewBulkStringCommand([]byte(element.Member))))
+		if withDist {
+			// 计算距离，写入结果数组
+			distance := geo.Distance(latitude, longitude, lat, lng)
+			nestedResult = append(nestedResult, redis.Encode(redis.NewSingleLineCommand([]byte(fmt.Sprintf("%.4f", distance)))))
+		}
+		if withHash {
+			// 获取Base32字符串，写入结果数组
+			nestedResult = append(nestedResult, redis.Encode(redis.NewBulkStringCommand([]byte(geo.ToString(buf)))))
+		}
+		if withCoord {
+			// 格式化经纬度
+			nestedResult = append(nestedResult, redis.Encode(redis.NewArrayCommand([][]byte{
+				[]byte(fmt.Sprintf("%.6f", lng)),
+				[]byte(fmt.Sprintf("%.6f", lat)),
+			})))
+		}
+		res := redis.Encode(redis.NewNestedArrayCommand(nestedResult))
+		result[i] = res
+	}
+	return redis.NewNestedArrayCommand(result)
+}
+
+func geoRadius0(sortedSet *zset.SortedSet, latitude, longitude float64, radiusMeters float64) []zset.Element {
+	var result []zset.Element
+	ranges := geo.AroundRadius(latitude, longitude, radiusMeters)
+	for _, rg := range ranges {
+		elements := sortedSet.RangeByScore(float64(rg[0]), float64(rg[1]), 0, sortedSet.Size(), false, false)
+		result = append(result, elements...)
+	}
+
+	return result
 }
