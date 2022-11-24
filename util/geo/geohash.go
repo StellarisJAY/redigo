b2dedd0ab66837608f28287dc1ebff204565f3cf
StellarisJAY <760409558@qq.com>
2022-11-24 11:46:51 +0800

    feat: 提高geohash精度到64bit

--- util/geo/geohash.go
+++ util/geo/geohash.go
@@ -2,6 +2,7 @@ package geo
 
 import (
 	"bytes"
+	"encoding/base32"
 	"encoding/binary"
 )
 
@@ -14,7 +15,7 @@ const (
 	base32Table          = "0123456789bcdefghjkmnpqrstuvwxyz"
 
 	// MaxPrecision 最大精度
-	MaxPrecision = 20
+	MaxPrecision = 32
 	// 纬度的最小单位跨度，即最小精度下的一个geoHash二分的纬度跨度
 	latitudeUnit float64 = 180.0 / (1 << MaxPrecision)
 	// 经度的最小单位跨度
@@ -23,6 +24,7 @@ const (
 
 var (
 	base32TableReverse = make(map[byte]byte)
+	enc                = base32.NewEncoding("0123456789bcdefghjkmnpqrstuvwxyz").WithPadding(base32.NoPadding)
 )
 
 func init() {
@@ -32,53 +34,37 @@ func init() {
 }
 
 // Encode 将坐标转换成geoHash
-func Encode(latitude, longitude float64, precision int) []byte {
-	if precision > MaxPrecision {
-		precision = MaxPrecision
-	}
-	latitudes := convert(minLatitude, maxLatitude, latitude, precision)
-	longitudes := convert(minLongitude, maxLongitude, longitude, precision)
+func Encode(latitude, longitude float64) []byte {
+	latitudes := convert(minLatitude, maxLatitude, latitude, MaxPrecision)
+	longitudes := convert(minLongitude, maxLongitude, longitude, MaxPrecision)
 	buffer := &bytes.Buffer{}
-	for i := 0; i < precision; i++ {
-		buffer.WriteByte(longitudes[i])
-		buffer.WriteByte(latitudes[i])
-	}
-	value := buffer.Bytes()
-	var result []byte
-	for i := 0; i < len(value); i += 5 {
+	for i := 0; i < MaxPrecision; i += 4 {
 		var sum byte = 0
-		for j := 0; j < 5; j++ {
-			sum += value[i+4-j] * (1 << j)
+		k := 0
+		for j := 7; j >= 0; j -= 2 {
+			sum += longitudes[i+k] * (1 << j)
+			sum += latitudes[i+k] * (1 << (j - 1))
+			k++
 		}
-		result = append(result, sum)
+		buffer.WriteByte(sum)
 	}
-	return result
+	return buffer.Bytes()
 }
 
 func Decode(value []byte) (float64, float64, error) {
-	var encoded []byte
-	for _, num := range value {
-		encoded = append(encoded, numberToBinary(num)...)
-	}
 	// 奇偶位分离出经度和纬度
 	var lats, lngs []byte
-	for i := 0; i < len(encoded); i++ {
-		if i&1 == 1 {
-			lats = append(lats, encoded[i])
-		} else {
-			lngs = append(lngs, encoded[i])
+	for _, num := range value {
+		for i := 7; i >= 0; i -= 2 {
+			lngs = append(lngs, num&(1<<i))
+			lats = append(lats, num&(1<<(i-1)))
 		}
 	}
-	// 转换回坐标值
 	return convertBack(minLatitude, maxLatitude, lats, len(lats)), convertBack(minLongitude, maxLongitude, lngs, len(lngs)), nil
 }
 
 func ToString(buffer []byte) string {
-	result := &bytes.Buffer{}
-	for _, b := range buffer {
-		result.WriteByte(base32Table[b])
-	}
-	return string(result.Bytes())
+	return enc.EncodeToString(buffer)
 }
 
 func FormatUint64(buffer []byte) uint64 {
@@ -127,15 +113,15 @@ func convertBack(min, max float64, value []byte, length int) float64 {
 // Around 坐标所在的geoHash块的周围最小精度范围内的所有块
 func Around(latitude, longitude float64) [][]byte {
 	var result [][]byte
-	result = append(result, Encode(latitude, longitude, MaxPrecision))
-	result = append(result, Encode(latitude+latitudeUnit, longitude, MaxPrecision))
-	result = append(result, Encode(latitude-latitudeUnit, longitude, MaxPrecision))
-	result = append(result, Encode(latitude, longitude+longitudeUnit, MaxPrecision))
-	result = append(result, Encode(latitude, longitude-longitudeUnit, MaxPrecision))
-	result = append(result, Encode(latitude+latitudeUnit, longitude+longitudeUnit, MaxPrecision))
-	result = append(result, Encode(latitude+latitudeUnit, longitude-longitudeUnit, MaxPrecision))
-	result = append(result, Encode(latitude-latitudeUnit, longitude+longitudeUnit, MaxPrecision))
-	result = append(result, Encode(latitude-latitudeUnit, longitude-longitudeUnit, MaxPrecision))
+	result = append(result, Encode(latitude, longitude))
+	result = append(result, Encode(latitude+latitudeUnit, longitude))
+	result = append(result, Encode(latitude-latitudeUnit, longitude))
+	result = append(result, Encode(latitude, longitude+longitudeUnit))
+	result = append(result, Encode(latitude, longitude-longitudeUnit))
+	result = append(result, Encode(latitude+latitudeUnit, longitude+longitudeUnit))
+	result = append(result, Encode(latitude+latitudeUnit, longitude-longitudeUnit))
+	result = append(result, Encode(latitude-latitudeUnit, longitude+longitudeUnit))
+	result = append(result, Encode(latitude-latitudeUnit, longitude-longitudeUnit))
 	return result
 }
 
