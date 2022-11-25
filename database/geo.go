7ab5ff0a5fe4881ec4351b85b1ab26a0a3507698
StellarisJAY <760409558@qq.com>
2022-11-25 11:22:18 +0800

    feat: GeoRadiusByMember命令；优化Radius相关命令实现

--- database/geo.go
+++ database/geo.go
@@ -5,8 +5,24 @@ import (
 	"redigo/datastruct/zset"
 	"redigo/redis"
 	"redigo/util/geo"
+	"sort"
 	"strconv"
+	"strings"
 )
+
+type radiusOptions struct {
+	unitFactor float64
+	withCoord  bool
+	withDist   bool
+	withHash   bool
+	sortAsc    bool
+	sortDesc   bool
+}
+
+type radiusResult struct {
+	encoded  []byte
+	distance float64
+}
 
 func init() {
 	RegisterCommandExecutor("GEOADD", execGeoAdd, -4)
@@ -14,6 +30,7 @@ func init() {
 	RegisterCommandExecutor("GEODIST", execGeoDist, -3)
 	RegisterCommandExecutor("GEOHASH", execGeoHash, -2)
 	RegisterCommandExecutor("GEORADIUS", execGeoRadius, -4)
+	RegisterCommandExecutor("GEORADIUSBYMEMBER", execGeoRadiusByMember, -3)
 }
 
 func execGeoAdd(db *SingleDB, command redis.Command) *redis.RespCommand {
@@ -180,37 +197,18 @@ func execGeoRadius(db *SingleDB, command redis.Command) *redis.RespCommand {
 	if (longitude < -180 || longitude > 180) || (latitude < -90 || latitude > 90) {
 		return redis.NewErrorCommand(redis.CreateInvalidCoordinatePairError(longitude, latitude))
 	}
-	var unitFactor float64 = 1
-	withCoord, withDist, withHash, _ := false, false, false, false
+	var options radiusOptions
 	if len(args) > 4 {
 		// 额外参数解析
-		extraArgs := args[4:]
-		for _, arg := range extraArgs {
-			switch string(arg) {
-			// 单位换算因子
-			case "m":
-				unitFactor = 1
-			case "km":
-				unitFactor = 1000
-			case "mi":
-				unitFactor = 1 / 0.00062137
-			case "ft":
-				unitFactor = 1 / 3.2808399
-			// 返回额外信息参数
-			case "WITHCOORD":
-				withCoord = true
-			case "WITHDIST":
-				withDist = true
-			case "WITHHASH":
-				withHash = true
-			case "DESC":
-				// todo 结果排序
-			default:
-				return redis.NewErrorCommand(redis.SyntaxError)
-			}
+		opt, err := parseRadiusArguments(args[4:])
+		if err != nil {
+			return redis.NewErrorCommand(err)
 		}
+		options = opt
+	} else {
+		options = radiusOptions{unitFactor: 1.0}
 	}
-	radius = radius * unitFactor
+	radius = radius * options.unitFactor
 	sortedSet, err := getSortedSet(db, key)
 	if err != nil {
 		return redis.NewErrorCommand(err)
@@ -219,44 +217,136 @@ func execGeoRadius(db *SingleDB, command redis.Command) *redis.RespCommand {
 		return redis.NilCommand
 	}
 	// 获取周围的元素
-	elements := geoRadius0(sortedSet, latitude, longitude, radius)
-	result := make([][]byte, len(elements))
-	for i, element := range elements {
+	return geoRadius0(sortedSet, latitude, longitude, radius, options)
+}
+
+func execGeoRadiusByMember(db *SingleDB, command redis.Command) *redis.RespCommand {
+	args := command.Args()
+	if !ValidateArgCount(command.Name(), len(args)) {
+		return redis.NewErrorCommand(redis.CreateWrongArgumentNumberError("GEORADIUS"))
+	}
+	key := string(args[0])
+	member := string(args[1])
+	// 解析范围半径参数
+	radius, err := strconv.ParseFloat(string(args[2]), 64)
+	if err != nil {
+		return redis.NewErrorCommand(redis.ValueNotFloatError)
+	}
+	var options radiusOptions
+	if len(args) > 3 {
+		// 额外参数解析
+		opt, err := parseRadiusArguments(args[3:])
+		if err != nil {
+			return redis.NewErrorCommand(err)
+		}
+		options = opt
+	} else {
+		options = radiusOptions{unitFactor: 1.0}
+	}
+	radius = radius * options.unitFactor
+	// 获取sortedSet
+	sortedSet, err := getSortedSet(db, key)
+	if err != nil {
+		return redis.NewErrorCommand(err)
+	}
+	if sortedSet == nil {
+		return redis.NilCommand
+	}
+	// 从zset获取member的geoHash值，decode出member的坐标
+	if element, ok := sortedSet.GetScore(member); !ok {
+		return redis.NilCommand
+	} else {
+		latitude, longitude, _ := geo.Decode(geo.FromUint64(uint64(element.Score)))
+		return geoRadius0(sortedSet, latitude, longitude, radius, options)
+	}
+}
+
+// 解析radius命令参数，返回选项和解析错误
+func parseRadiusArguments(args [][]byte) (radiusOptions, error) {
+	options := radiusOptions{}
+	for _, arg := range args {
+		switch strings.ToLower(string(arg)) {
+		// 单位换算因子
+		case "m":
+			options.unitFactor = 1
+		case "km":
+			options.unitFactor = 1000
+		case "mi":
+			options.unitFactor = 1 / 0.00062137
+		case "ft":
+			options.unitFactor = 1 / 3.2808399
+		// 返回额外信息参数
+		case "withcoord":
+			options.withCoord = true
+		case "withdist":
+			options.withDist = true
+		case "withhash":
+			options.withHash = true
+		case "desc":
+			options.sortDesc = true
+		case "asc":
+			options.sortAsc = true
+		default:
+			return options, redis.SyntaxError
+		}
+	}
+	return options, nil
+}
+
+func geoRadius0(sortedSet *zset.SortedSet, latitude, longitude float64, radiusMeters float64, options radiusOptions) *redis.RespCommand {
+	// 获得坐标周围的geoHash块
+	ranges := geo.AroundRadius(latitude, longitude, radiusMeters)
+	elements := make(map[string]float64)
+	// 从zset中找到所有符合ranges块范围的成员
+	for _, rg := range ranges {
+		elems := sortedSet.RangeByScore(float64(rg[0]), float64(rg[1]), 0, sortedSet.Size(), false, false)
+		for _, elem := range elems {
+			elements[elem.Member] = elem.Score
+		}
+	}
+	result := make([]radiusResult, 0, len(elements))
+	for member, score := range elements {
 		// 单个element的结果是一个嵌套的数组
 		var nestedResult [][]byte
-		buf := geo.FromUint64(uint64(element.Score))
+		buf := geo.FromUint64(uint64(score))
 		// 解码geoHash值
 		lat, lng, _ := geo.Decode(buf)
-		nestedResult = append(nestedResult, redis.Encode(redis.NewBulkStringCommand([]byte(element.Member))))
-		if withDist {
-			// 计算距离，写入结果数组
-			distance := geo.Distance(latitude, longitude, lat, lng)
-			nestedResult = append(nestedResult, redis.Encode(redis.NewSingleLineCommand([]byte(fmt.Sprintf("%.4f", distance)))))
+		nestedResult = append(nestedResult, redis.Encode(redis.NewBulkStringCommand([]byte(member))))
+		var distance float64
+		if options.withDist || options.sortAsc || options.sortDesc {
+			// 计算距离
+			distance = geo.Distance(latitude, longitude, lat, lng) / options.unitFactor
+			if options.withDist {
+				nestedResult = append(nestedResult, redis.Encode(redis.NewSingleLineCommand([]byte(fmt.Sprintf("%.4f", distance)))))
+			}
 		}
-		if withHash {
+		if options.withHash {
 			// 获取Base32字符串，写入结果数组
 			nestedResult = append(nestedResult, redis.Encode(redis.NewBulkStringCommand([]byte(geo.ToString(buf)))))
 		}
-		if withCoord {
+		if options.withCoord {
 			// 格式化经纬度
 			nestedResult = append(nestedResult, redis.Encode(redis.NewArrayCommand([][]byte{
-				[]byte(fmt.Sprintf("%.6f", lng)),
-				[]byte(fmt.Sprintf("%.6f", lat)),
+				[]byte(fmt.Sprintf("%f", lng)),
+				[]byte(fmt.Sprintf("%f", lat)),
 			})))
 		}
 		res := redis.Encode(redis.NewNestedArrayCommand(nestedResult))
-		result[i] = res
+		result = append(result, radiusResult{distance: distance, encoded: res})
 	}
-	return redis.NewNestedArrayCommand(result)
-}
-
-func geoRadius0(sortedSet *zset.SortedSet, latitude, longitude float64, radiusMeters float64) []zset.Element {
-	var result []zset.Element
-	ranges := geo.AroundRadius(latitude, longitude, radiusMeters)
-	for _, rg := range ranges {
-		elements := sortedSet.RangeByScore(float64(rg[0]), float64(rg[1]), 0, sortedSet.Size(), false, false)
-		result = append(result, elements...)
+	// 按照距离值升序或降序排序
+	if options.sortAsc {
+		sort.Slice(result, func(i, j int) bool {
+			return result[i].distance < result[j].distance
+		})
+	} else if options.sortDesc {
+		sort.Slice(result, func(i, j int) bool {
+			return result[i].distance > result[j].distance
+		})
 	}
-
-	return result
+	arrElements := make([][]byte, len(result))
+	for i, res := range result {
+		arrElements[i] = res.encoded
+	}
+	return redis.NewNestedArrayCommand(arrElements)
 }
