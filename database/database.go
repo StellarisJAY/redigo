af2918e3b25f97a3c1fb5a9522447d87ec0394eb
StellarisJAY <760409558@qq.com>
2023-02-13 11:52:19 +0800

    perf: 使用fork优化bgsave，避免数据拷贝

--- database/database.go
+++ database/database.go
@@ -358,30 +358,5 @@ func (m *MultiDB) execBGSave(command redis.Command) *redis.RespCommand {
 	if !m.aofHandler.RewriteStarted.CompareAndSwap(false, true) {
 		return redis.NewErrorCommand(redis.BackgroundSaveInProgressError)
 	}
-	startTime := time.Now()
-	// get the snapshot of current memory
-	snapshot := make([][]*rdb.DataEntry, config.Properties.Databases)
-	for i := 0; i < config.Properties.Databases; i++ {
-		size := m.dbSet[i].Len(i)
-		entries := make([]*rdb.DataEntry, size)
-		snapshot[i] = entries
-		j := 0
-		m.ForEach(i, func(key string, entry *database.Entry, expire *time.Time) bool {
-			entries[j] = &rdb.DataEntry{Key: key, Value: entry.Data, ExpireTime: expire}
-			j++
-			return true
-		})
-	}
-	// run save in background
-	go func(entries [][]*rdb.DataEntry, startTime time.Time) {
-		// release rewrite lock
-		defer m.aofHandler.RewriteStarted.Store(false)
-		err := rdb.BGSave(entries)
-		if err != nil {
-			log.Errorf("BGSave RDB error: %v", err)
-		} else {
-			log.Info("BGSave RDB finished: %d ms", time.Now().Sub(startTime).Milliseconds())
-		}
-	}(snapshot, startTime)
-	return redis.NewSingleLineCommand([]byte("Background saving started"))
+	return BGSave(m, command)
 }
